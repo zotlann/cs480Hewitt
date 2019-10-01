920c2060ea308e5b82fe53a7495f02871d45d4e7
zotlann <jhewittjrs@gmail.com>
2019-10-01 18:35:56 +0000

    added support for scales in cmd arg

--- PA5/include/assets.h
+++ PA5/include/assets.h
@@ -5,6 +5,11 @@ struct ShaderFiles{
 	char* vshader_filename;
 	char* fshader_filename;
 	char* config_filename;
+	char* obj_filename;
+	float scale;
+	ShaderFiles(){
+	  scale = 0.0;
+	}
 };
 
 #endif
