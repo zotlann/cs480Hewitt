10cc1a964332af3596c1d364ce32841a08164a89
Sidney <49454007+sinouye@users.noreply.github.com>
2019-10-01 20:18:17 -0700

    Model loader now fully works

---
+++ PA5/PA5/include/shader.h
@@ -0,0 +1,26 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <vector>
+#include <fstream>
+#include "assets.h"
+
+#include "graphics_headers.h"
+
+class Shader
+{
+  public:
+    Shader();
+    ~Shader();
+    bool Initialize();
+    void Enable();
+    bool AddShader(GLenum ShaderType,char* filename);
+    bool Finalize();
+    GLint GetUniformLocation(const char* pUniformName);
+
+  private:
+    GLuint m_shaderProg;    
+    std::vector<GLuint> m_shaderObjList;
+};
+
+#endif  /* SHADER_H */
