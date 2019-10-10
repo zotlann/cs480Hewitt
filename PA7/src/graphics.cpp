4f5591fdc7f10a18700f47bc9ee00b4a5a7de8ed
zotlann <jhewittjrs@gmail.com>
2019-10-10 15:39:23 +0000

    removed debugging print statements

--- PA7/src/graphics.cpp
+++ PA7/src/graphics.cpp
@@ -45,7 +45,6 @@ bool Graphics::Initialize(int width, int height, Config cfg)
   }
   //Create the object
   m_cube =  new Object(cfg.root_planet_filename);
-  std::cout << "ORBIT_SCALE: " << Object::orbit_scale << std::endl;
   //set up the objects vector
   objects.clear();
   objects.push_back(m_cube);
