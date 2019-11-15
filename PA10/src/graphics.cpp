bc1c398c8f73b3461deebd2260bfebe18f283be4
Sidney Inouye <sinouye@nevada.unr.edu>
2019-11-14 17:54:15 -0800

    gravity works. love, sidney

--- PA10/src/graphics.cpp
+++ PA10/src/graphics.cpp
@@ -72,7 +72,7 @@ bool Graphics::Initialize(int width, int height, Config cfg)
   dynamics_world = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collision_config );
 
   //set gravity
-  dynamics_world->setGravity(btVector3(0,-10,0));
+  dynamics_world->setGravity(btVector3(10,-10,0));
 
   //Create the objects
   m_table = new Table(cfg.table_config);
